da88a5f3cebf5e24abe8ea8e94a6b1635dcf8b3d
darknius20 <darknius21@gmail.com>
2022-10-18 10:22:26 +0000

    juice: initialize libperfmgr

--- device.mk
+++ device.mk
@@ -181,10 +181,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.qti \
-    vendor.qti.hardware.perf@2.0 \
-    vendor.qti.hardware.perf@2.1 \
-    vendor.qti.hardware.perf@2.2
+    android.hardware.power@1.3.vendor \
+    android.hardware.power-service.juice-libperfmgr \
+    libqti-perfd-client
+
 
 # Radio
 PRODUCT_PACKAGES += \
