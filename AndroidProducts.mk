246ce828b90631a5f83ec27992840c5be2a0d6be
darknius09 <darkniuslightness@gmail.com>
2022-11-20 16:56:58 +0000

    AICP BRINGUP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The fluidOS Project
+# Copyright (C) 2021 The aicpOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fluid_juice.mk
+    $(LOCAL_DIR)/aicp_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    fluid_juice-user \
-    fluid_juice-userdebug \
-    fluid_juice-eng
+    aicp_juice-user \
+    aicp_juice-userdebug \
+    aicp_juice-eng
