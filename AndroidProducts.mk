5973157471beb68a1f5ca09c5b29f6986e6601a4
darknius09 <darkniuslightness@gmail.com>
2023-01-14 15:40:40 +0000

    fluid bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The revengeos Project
+# Copyright (C) 2021 The fluid Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/revengeos_juice.mk
+    $(LOCAL_DIR)/fluid_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    revengeos_juice-user \
-    revengeos_juice-userdebug \
-    revengeos_juice-eng
+    fluid_juice-user \
+    fluid_juice-userdebug \
+    fluid_juice-eng
