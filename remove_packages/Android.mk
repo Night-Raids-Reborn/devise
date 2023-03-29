d1e4428289901a3cf8c3086e1edc8c508aa3a5be
darknius09 <darkniuslightness@gmail.com>
2023-03-29 00:01:44 +0000

    juice: overide NexusLauncherRelease

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService arcore
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService arcore NexusLauncherRelease
 LOCAL_OVERRIDES_PACKAGES += CarrierSetup ConnMO ConnMetrics
 LOCAL_OVERRIDES_PACKAGES += DCMO DevicePolicyPrebuilt DMService
 LOCAL_OVERRIDES_PACKAGES += GCS GoogleCamera
